05a06bd6adbfc970fea441f98b8f3160e673227d
hiteshgarg4000 <hiteshgarg4000@gmail.com>
2021-06-08 07:26:07 +0530

    trying to create a sign up

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,12 @@
 //require the library
 const mongoose = require('mongoose');
-
+//connect to the database
+mongoose.connect('mongodb://localhost/codeial_development_db');
+// acqire the connection (to check if it is successfull)
+const db = mongoose.connection;
+//error
+db.on('error',console.error.bind(console,'error connection to db'));
+// up and running then print the message
+db.once('open',function(){
+    console.log('Succesfully connected to the  database');
+});
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/users');
+
 module.exports.profile = function(req,res){
     return res.render('user_profile',{title:"User Profile"});
 }
@@ -8,4 +10,27 @@ module.exports.signUp=function(req,res){
 //render the sign in page
 module.exports.signIn=function(req,res){
     return res.render('user_sign_in',{title:"Codeial | Sign In"});
+}
+//get the sign up data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password)
+    return res.redirect('back');
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing up');return;}
+    
+    if(!User){
+        User.create(req.body,function(err,user){
+            if(err){console.log('error in creating a user');return;};
+            return res.redirect('/users/sign-in');
+        });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+    
+}
+//sign and create data
+module.exports.createSession = function(req,res){
+    //TODO later
 }
