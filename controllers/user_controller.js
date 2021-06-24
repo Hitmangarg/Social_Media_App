9d199795fcc1c71d80b2186fe922a73e84e298c7
hiteshgarg4000 <hiteshgarg4000@gmail.com>
2021-06-24 19:32:53 +0530

    adding a list of friends

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,8 +1,12 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{title:"User Profile"});
+    //return res.render('user_profile',{title:"User Profile"});
+    User.findById(req.params.id,function(err,user){
+        return res.render('user_profile',{title:'User Profile',profile_user :user});
+    });
 }
+
 // render the sign up page
 module.exports.signUp=function(req,res){
     if(req.isAuthenticated()){
