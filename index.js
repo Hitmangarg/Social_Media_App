a9b032e5af3a83600b038d450308ab0fde34e057
hiteshgarg4000 <hiteshgarg4000@gmail.com>
2021-06-25 14:13:43 +0530

    use flash in app

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(passport.setAuthenticatedUser);
+app.use(flash());
 //use express router
 app.use('/',require('./routes')); 
 app.listen(port,function(err){
