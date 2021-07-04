88d963f61a31eddc700facecdd8d4b8e0400e10e
hiteshgarg4000 <hiteshgarg4000@gmail.com>
2021-07-04 18:45:31 +0530

    seeing the detalis of use using postman api

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,6 +1,12 @@
 const express = require('express');
- 
+
 const router = express.Router();
-const postApi = require('../../../controllers/api/v1/post_api');
-router.get('/',postApi.index);
+const postsApi = require("../../../controllers/api/v1/posts_api");
+
+
+router.get('/', postsApi.index);
+router.delete('/:id', postsApi.destroy);
+
+
+
 module.exports = router;
